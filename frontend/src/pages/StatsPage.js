248ca24dc9756c04c3cce2f0888be7f0c9fb4c4c
Jonathan Jones <jonathan.jones1@ufl.edu>
2024-11-09 17:51:02 -0500

    Created the stats page.

---
+++ frontend/src/pages/StatsPage.js
@@ -0,0 +1,10 @@
+const StatsPage = () => {
+    return(
+        <div className = "StatsPage">
+            <h1>What the actual frick bro?</h1>
+            <p>I have no idea what's going on.</p>
+        </div>
+    )
+}
+
+export default StatsPage
