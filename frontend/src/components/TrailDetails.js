8d0bb26c2ca8e2a4ac9419ba58ead6dbc9c9674b
Jonathan Jones <jonathan.jones1@ufl.edu>
2024-11-09 21:03:05 -0500

    Imported DisplayDate to neatly format the date.

--- frontend/src/components/TrailDetails.js
+++ frontend/src/components/TrailDetails.js
@@ -1,8 +1,10 @@
+import DisplayDate from "./DisplayDate"
+
 const TrailDetails = ({trail}) => {
 
     return(
         <div className = "TrailDetails">
-            <h4>{trail.date}</h4>
+            <h4>{<DisplayDate dateString={trail.date} />}</h4>
             <p><strong>DURATION:</strong>{trail.duration}</p>
             <p><strong>TIME:</strong>{trail.quality}</p>
             <p>{trail.createdAt}</p>
